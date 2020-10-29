1b104527ac9ac003b194fb55dbef8c205a9a2fd5
aitor <aitorambite@protonmail.com>
2020-10-29 12:12:21 +0100

    Initial commit

---
+++ src/Cliente.java
@@ -0,0 +1,18 @@
+public class Cliente extends Thread {
+    @Override
+    public void run() {
+        try {
+            if (Main.list.isEmpty()){
+                System.out.println("Cliente - Voy a esperar a que me traigan comida...");
+            } else {
+                while(!Main.list.isEmpty()){
+                    System.out.println("Cliente - Voy a consumir un " + Main.list.get(0) + " 3 segundos");
+                    Main.list.remove(0);
+                    sleep(3000);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
